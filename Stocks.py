b9aea0ef99a713123789a9ddbf011170095ce8bb
arpittiwari11 <82596857+arpittiwari11@users.noreply.github.com>
2021-06-27 23:56:21 +0530

    Add files via upload

---
+++ Stocks.py
@@ -0,0 +1,462 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import math
+import numpy as np
+from nsepy import get_history
+from datetime import date
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+
+# In[2]:
+
+
+import os
+import warnings
+warnings.filterwarnings('ignore')
+from pylab import rcParams
+rcParams['figure.figsize'] = 10, 6
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.arima_model import ARIMA
+from pmdarima.arima import auto_arima
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+import pmdarima as pm
+
+
+# In[4]:
+
+
+sbin = get_history(symbol='SBIN',
+                   start=date(2000,1,1),
+                   end=date(2020,11,1))
+
+
+# In[ ]:
+
+
+
+
+
+# In[5]:
+
+
+sbin.shape
+
+
+# In[7]:
+
+
+sbin.head(5)
+
+
+# In[8]:
+
+
+plt.figure(figsize=(10,6))
+plt.grid(True)
+plt.xlabel('Dates')
+plt.ylabel('Close Prices')
+plt.plot(sbin['Close'])
+plt.title('SBIN closing price')
+plt.show()
+
+
+# In[9]:
+
+
+plt.figure(figsize=(10,6))
+df_close = sbin['Close']
+df_close.plot(style='k.')
+plt.title('Scatter plot of closing price')
+plt.show()
+
+
+# In[10]:
+
+
+plt.figure(figsize=(10,6))
+df_close = sbin['Close']
+df_close.plot(style='k.',kind='hist')
+plt.title('Hisogram of closing price')
+plt.show()
+
+
+# In[11]:
+
+
+from statsmodels.tsa.stattools import adfuller
+def test_stationarity(timeseries):
+    #Determing rolling statistics
+    rolmean = timeseries.rolling(12).mean()
+    rolstd = timeseries.rolling(12).std()
+    #Plot rolling statistics:
+    plt.plot(timeseries, color='yellow',label='Original')
+    plt.plot(rolmean, color='red', label='Rolling Mean')
+    plt.plot(rolstd, color='black', label = 'Rolling Std')
+    plt.legend(loc='best')
+    plt.title('Rolling Mean and Standard Deviation')
+    plt.show(block=False)
+    print("Results of dickey fuller test")
+    adft = adfuller(timeseries,autolag='AIC')
+    # output for dft will give us without defining what the values are.
+    #hence we manually write what values does it explains using a for loop
+    output = pd.Series(adft[0:4],index=['Test Statistics','p-value','No. of lags used','Number of observations used'])
+    for key,values in adft[4].items():
+        output['critical value (%s)'%key] =  values
+    print(output)
+
+test_stationarity(sbin['Close'])
+
+
+# In[12]:
+
+
+result = seasonal_decompose(df_close, model='multiplicative', freq = 30)
+fig = plt.figure()  
+fig = result.plot()  
+fig.set_size_inches(16, 9)
+
+
+# In[13]:
+
+
+from pylab import rcParams
+rcParams['figure.figsize'] = 10, 6
+df_log = np.log(sbin['Close'])
+#df_log =sbin['Close']
+moving_avg = df_log.rolling(12).mean()
+std_dev = df_log.rolling(12).std()
+plt.legend(loc='best')
+plt.title('Moving Average')
+plt.plot(std_dev, color ="black", label = "Standard Deviation")
+plt.plot(moving_avg, color="red", label = "Mean")
+plt.legend()
+plt.show()
+
+
+# In[14]:
+
+
+train_data, test_data = df_log[3:int(len(df_log)*0.9)], df_log[int(len(df_log)*0.9):]
+plt.figure(figsize=(10,6))
+plt.grid(True)
+plt.xlabel('Dates')
+plt.ylabel('Closing Prices')
+plt.plot(df_log, 'green', label='Train data')
+plt.plot(test_data, 'blue', label='Test data')
+plt.legend()
+
+
+# In[15]:
+
+
+model_autoARIMA = auto_arima(train_data, start_p=0, start_q=0,
+test='adf',       # use adftest to find optimal 'd'
+max_p=3, max_q=3, # maximum p and q
+m=1,              # frequency of series
+d=None,           # let model determine 'd'
+seasonal=False,   # No Seasonality
+start_P=0, 
+D=0, 
+trace=True,
+error_action='ignore',  
+suppress_warnings=True, 
+stepwise=True)
+print(model_autoARIMA.summary())
+
+
+# In[16]:
+
+
+model_autoARIMA.plot_diagnostics(figsize=(15,8))
+plt.show()
+
+
+# In[27]:
+
+
+warnings.filterwarnings("ignore")
+model = ARIMA(train_data, order=(3, 1, 2))
+fitted = model.fit(disp=-1)
+print(fitted.summary())
+
+
+# In[28]:
+
+
+# Forecast
+
+fc, se, conf = fitted.forecast(519, alpha=0.05)  # 95% confidence
+
+
+# In[100]:
+
+
+fc_series = pd.Series(fc, index=test_data.index)
+lower_series = pd.Series(conf[:, 0], index=test_data.index)
+upper_series = pd.Series(conf[:, 1], index=test_data.index)
+plt.figure(figsize=(12,5), dpi=100)
+#plt.plot(train_data, label='training')
+plt.plot(test_data, color = 'blue', label='Actual Stock Price')
+plt.plot(fc_series, color = 'orange',label='Predicted Stock Price')
+plt.fill_between(lower_series.index, lower_series, upper_series, 
+                 color='k', alpha=.10)
+plt.title('SBIN Stock Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('Actual Stock Price')
+plt.legend(loc='upper left', fontsize=8)
+plt.show()
+
+
+# In[30]:
+
+
+# report performance
+mse = mean_squared_error(test_data, fc)
+print('MSE: '+str(mse))
+mae = mean_absolute_error(test_data, fc)
+print('MAE: '+str(mae))
+rmse = math.sqrt(mean_squared_error(test_data, fc))
+print('RMSE: '+str(rmse))
+mape = np.mean(np.abs(fc - test_data)/np.abs(test_data))
+print('MAPE: '+str(mape))
+
+
+# In[31]:
+
+
+import sys
+print(sys.executable)
+print(sys.version)
+print(sys.version_info)
+
+
+# In[32]:
+
+
+from keras.models import Sequential
+from keras.layers import Dense,Dropout, Flatten,LSTM,RepeatVector,TimeDistributed,Conv1D,MaxPooling1D
+from keras.callbacks import EarlyStopping
+#from keras.optimizers import Adam
+from tensorflow.keras.optimizers import Adam
+
+from livelossplot.keras import PlotLossesCallback
+
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+
+# In[37]:
+
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn import model_selection
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+
+# In[36]:
+
+
+sbin.head()
+
+
+# In[43]:
+
+
+sbin['Date'] = sbin.index
+sbin.head()
+
+
+# In[40]:
+
+
+data2 = pd.DataFrame(columns = ['Date', 'Open', 'High', 'Low', 'Close'])
+data2['Date'] = sbin['Date']
+data2['Open'] = sbin['Open']
+data2['High'] = sbin['High']
+data2['Low'] = sbin['Low']
+data2['Close'] = sbin['Close']
+
+
+# In[41]:
+
+
+data2.head()
+
+
+# In[44]:
+
+
+train_set = data2.iloc[:, 1:2].values
+
+
+# In[47]:
+
+
+train_set.shape
+
+
+# In[48]:
+
+
+data2.shape
+
+
+# In[49]:
+
+
+train_set
+
+
+# In[50]:
+
+
+sc = MinMaxScaler(feature_range = (0, 1))
+training_set_scaled = sc.fit_transform(train_set)
+
+
+# In[69]:
+
+
+X_train = []
+y_train = []
+for i in range(60, 5186):
+    X_train.append(training_set_scaled[i-60:i, 0])
+    y_train.append(training_set_scaled[i, 0]) 
+X_train, y_train = np.array(X_train), np.array(y_train)
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+
+
+# In[54]:
+
+
+""""
+X_train = []
+y_X_train.append(training_set_scaled[i-60:i, 0])
+y_train.append(training_set_scaled[i, 0])train = []
+
+i=60
+ X_train
+    
+"""    
+
+
+# In[101]:
+
+
+regressor = Sequential()
+regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+regressor.add(Dropout(0.05))
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.05))
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.05))
+regressor.add(LSTM(units = 50))
+regressor.add(Dropout(0.05))
+regressor.add(Dense(units = 1))
+
+
+# In[102]:
+
+
+regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
+
+
+# In[103]:
+
+
+testdataframe = get_history(symbol='SBIN',
+                   start=date(2020,11,2),
+                   end=date(2021,11,1))
+
+
+# In[104]:
+
+
+testdataframe.head()
+
+
+# In[105]:
+
+
+testdataframe.shape
+
+
+# In[106]:
+
+
+testdataframe['Date'] = testdataframe.index
+testdata = pd.DataFrame(columns = ['Date', 'Open', 'High', 'Low', 'Close'])
+testdata['Date'] = testdataframe['Date']
+testdata['Open'] = testdataframe['Open']
+testdata['High'] = testdataframe['High']
+testdata['Low'] = testdataframe['Low']
+testdata['Close'] = testdataframe['Close']
+
+real_stock_price = testdata.iloc[:, 1:2].values
+
+dataset_total = pd.concat((data2['Open'], testdata['Open']), axis = 0)   #Total dataset#
+
+inputs = dataset_total[len(dataset_total) - len(testdata) - 60:].values
+inputs = inputs.reshape(-1,1)
+inputs = sc.transform(inputs)
+X_test = []
+for i in range(60, 220):
+    X_test.append(inputs[i-60:i, 0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+
+
+# In[107]:
+
+
+predicted_stock_price = regressor.predict(X_test)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+
+
+# In[108]:
+
+
+plt.figure(figsize=(20,10))
+plt.plot(real_stock_price, color = 'green', label = 'SBI Stock Price')
+plt.plot(predicted_stock_price, color = 'red', label = 'Predicted SBI Stock Price')
+plt.title('SBI Stock Price Prediction')
+plt.xlabel('Trading Day')
+plt.ylabel('SBI Stock Price')
+plt.legend()
+plt.show()
+
+
+# In[98]:
+
+
+#Previous
+plt.figure(figsize=(20,10))
+plt.plot(real_stock_price, color = 'green', label = 'SBI Stock Price')
+plt.plot(predicted_stock_price, color = 'red', label = 'Predicted SBI Stock Price')
+plt.title('SBI Stock Price Prediction')
+plt.xlabel('Trading Day')
+plt.ylabel('SBI Stock Price')
+plt.legend()
+plt.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
